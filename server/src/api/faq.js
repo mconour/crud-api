17bd4090b3e5a63df154a8f5bb0c4e1aafe754ee
Michael Conour <michaelconour@gmail.com>
2020-09-09 17:02:07 -0700

    set up routes for reading, creating, updating, and deleting

--- server/src/api/faq.js
+++ server/src/api/faq.js
@@ -1,2 +1,43 @@
 const express = require('express');
+
+// create a router
 const router = express.Router();
+
+// read all
+router.get('/', (req, res, next) => {
+    res.json({
+        message: `read all`,
+    });
+});
+
+// read one
+router.get('/:id', (req, res, next) => {
+    res.json({
+        message: `read one`,
+    });
+});
+
+// create 
+router.post('/', (req, res, next) => {
+    res.json({
+        message: `create one`,
+    });
+});
+
+// update 
+router.put('/:id', (req, res, next) => {
+    res.json({
+        message: `update one`,
+    });
+});
+
+// delete
+router.delete('/:id', (req, res, next) => {
+    res.json({
+        message: `delete one`,
+    });
+});
+
+
+
+module.exports = router;
