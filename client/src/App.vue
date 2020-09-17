bdf6f298c730ba72543b7ad45e14344f32efcdf2
Michael Conour <michaelconour@gmail.com>
2020-09-16 19:12:42 -0700

    start createFAQS function && implement form on Create.vue page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
 <div id="app">
     <Navbar />
-    <div class="container">
+    <div class="container mt-3">
         <router-view />
     </div>
 </div>
--- client/src/views/Create.vue
+++ client/src/views/Create.vue
@@ -1,3 +1,55 @@
 <template>
-<div>Create!</div>
+<form>
+    <div class="field">
+        <label class="label">Question</label>
+        <div class="control">
+            <input v-model="question" class="input" type="text" name="question" placeholder="e.g How?" required>
+        </div>
+    </div>
+
+    <div class="field">
+        <label class="label">Answer</label>
+        <div class="control">
+            <textarea v-model="answer" class="textarea" rows="4" type="text" placeholder="e.g. Because."></textarea>
+        </div>
+    </div>
+    <button type="submit" class="button is-warning">Create</button>
+</form>
 </template>
+
+<script>
+import { ref } from '@vue/composition-api';
+
+export default {
+  setup() {
+    const question = ref('');
+    const answer = ref('');
+    const API_URL = 'http://localhost:5000/api/v1/faqs';
+
+    async function createFAQS() {
+      const response = await fetch(API_URL, {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          question: question.value,
+          answer: answer.value,
+        }),
+      });
+      const json = await response.json();
+      if (response.ok) {
+        // redirect!
+      } else {
+        // show an error
+      }
+    }
+
+    return {
+      question,
+      answer,
+      createFAQ,
+    };
+  },
+};
+</script>
