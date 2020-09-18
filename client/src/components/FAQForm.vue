26f39192d97bf37951692531ecd78e5de08fb12f
Michael Conour <michaelconour@gmail.com>
2020-09-17 18:53:12 -0700

    create form component

---
+++ client/src/components/FAQForm.vue
@@ -0,0 +1,32 @@
+<template>
+<!-- eslint-disable max-len -->
+<form @submit="submitForm">
+    <div class="field">
+        <label class="label">Question</label>
+        <div class="control">
+    <input v-model="question" class="input" type="text" name="question" placeholder="e.g How?" required>
+        </div>
+    </div>
+
+    <div class="field">
+        <label class="label">Answer</label>
+        <div class="control">
+    <textarea v-model="answer" name="answer" class="textarea" rows="4" placeholder="e.g. Because."></textarea>
+</div>
+    </div>
+    <button type="submit" class="button is-warning">Create</button>
+</form>
+</template>
+
+<script>
+export default {
+  props: ['submitForm', 'faq'],
+  setup({ props }) {
+    return {
+      question: props.faq.question,
+      answer: props.faq.answer,
+      submitForm: props.submitForm,
+    };
+  },
+};
+</script>
--- client/src/views/Create.vue
+++ client/src/views/Create.vue
@@ -1,21 +1,6 @@
 <template>
 <!-- eslint-disable max-len -->
-<form @submit="createFAQ">
-    <div class="field">
-        <label class="label">Question</label>
-        <div class="control">
-    <input v-model="question" class="input" type="text" name="question" placeholder="e.g How?" required>
-        </div>
-    </div>
-
-    <div class="field">
-        <label class="label">Answer</label>
-        <div class="control">
-    <textarea v-model="answer" name="answer" class="textarea" rows="4" placeholder="e.g. Because."></textarea>
-</div>
-    </div>
-    <button type="submit" class="button is-warning">Create</button>
-</form>
+   <FAQForm :faq="faq" :submitForm="createFAQ" />
 </template>
 
 <script>
